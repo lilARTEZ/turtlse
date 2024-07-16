b55be59e41d44a0d73f8d5bce9e7cd5c4358a150
lilARTEZ <85436631+lilARTEZ@users.noreply.github.com>
2024-07-16 16:03:06 +0200

    Update Main_turtle.lua

--- Main_turtle.lua
+++ Main_turtle.lua
@@ -1,4 +1,13 @@
 local locationFile='location.txt'
+local directiveFile='directive.txt'
+local actionFile='action.txt'
+
+
+local location={{0,0,0},{0,' - facing Z'}}
+local directive={"Inquisitor"}
+local action={}
+
+
 
 
 local function fileExists(name)
@@ -14,7 +23,7 @@ end
 
 
 
-local function writeFile(path,data)--list of rows to write
+local function writeFile(path,data)--list of rows to write {data,data,data}
     local file = io.open(path, 'w')
     for index, value in ipairs(data) do
         file:write(value..'\n')
@@ -52,20 +61,127 @@ local function refuel()
             turtle.select()
             turtle.refuel()
         end
-    end    
+    end
 end
 
 
 
-local function move(direction)
+local function turn(location,direction)
+
+    local facing=location[2][1]
+
+
     if direction=='left' then
+        facing=facing-1
+        if facing<-2 then
+            facing=1
+        end
         turtle.turnLeft()
     elseif direction=='right' then
+        facing=facing+1
+        if facing>2 then
+            facing=-1
+        end
         turtle.turnRight()
-    elseif direction=='back' then
-        turtle.turnLeft()
-        turtle.turnLeft()
+    end
+
+
+    if facing==0 then
+        location[2]={facing,' - facing Z'}
+    elseif facing==1 then
+        location[2]={facing,' - facing X'}
+    elseif facing==-1 then
+        location[2]={facing,' - facing -X'}
     else
-        print("wrong move direction")
+        location[2]={facing,' - facing -Z'}
+    end
+
+    writeFile(locationFile,location)
+    return location
+end
+
+
+local function turnTo(location,direction)
+    while location[2][1]~=direction do
+        location = turn(location,'left')
+    end
+end
+
+
+
+local function goToPath(location,destiny)
+    local position=location[1]
+    local distance = {0,0,0}
+
+    local path={'move'}
+
+    for index, value in ipairs(location) do
+        distance[index]=destiny[index]-value
+    end
+
+
+    
+    while position[2]~= destiny[2] do
+        if distance[2]<0 then
+            position[2]=position[2]-1
+            table.insert(path,-3)
+        else
+            position[2]=position[2]+1
+            table.insert(path,3)
+        end
+    end
+    while position[1]~= destiny[1] do
+        if distance[1]<0 then
+            position[1]=position[1]-1
+            table.insert(path,-1)
+        else
+            position[1]=position[1]+1
+            table.insert(path,1)
+        end
+    end
+    while position[3]~= destiny[3] do
+        if distance[3]<0 then
+            position[3]=position[3]-1
+            table.insert(path,2)
+        else
+            position[3]=position[3]+1
+            table.insert(path,0)
+        end
+    end
+    writeFile(actionFile,{path})
+    action = path
+end
+
+
+
+
+
+local function goTo()
+    location = readFile(locationFile)
+    action = readFile(actionFile)
+    for i = 2, #action, 1 do
+        if action[i]==5 or action[i]==-5 then
+            
+        end
+    end
+end
+
+
+
+
+if not fileExists(locationFile) then
+    writeFile(locationFile,location)
+end
+if not fileExists(directiveFile) then
+    writeFile(directiveFile,directive)
+end
+if not fileExists(actionFile) then
+    writeFile(actionFile,action)
+else
+    action=readFile(actionFile)
+    if action[1]~='' then
+        if action[1]=='move' then
+            goTo()
+        end
     end
 end
